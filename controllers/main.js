938dc62c990688fcaa785fef949638c94e50b0f2
Emmanuel Zelarayan <emmavz@live.com>
2022-07-25 15:14:13 -0300

    error handler refactorization

--- controllers/main.js
+++ controllers/main.js
@@ -4,7 +4,8 @@
 
 // setup authentication so only the request with JWT can access the dashboard
 
-const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -13,17 +14,25 @@ const login = async (req, res) => {
   // Joi
   // check in the controller
 
-  if(!username || !password){
-    throw new CustomAPIError("Please provide email and password", 400);
+  if (!username || !password) {
+    throw new BadRequestError("Please provide email and password");
   }
 
-  res.send("Fake Login/Register/Signup Route");
+  // just for demo, normally provided by DB!!!
+  const id = new Date().getDate();
+
+  // try to keep payload small, better exprecience for user
+  // just for demo, in production use long, complex and unguessable string value to the sign!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: "30d" });
+
+  res.status(200).json({ msg: "Use created", token });
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
+
   res.status(200).json({
-    msg: `Hello, John Doe`,
+    msg: `Hello, ${req.user.username}`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
 };
